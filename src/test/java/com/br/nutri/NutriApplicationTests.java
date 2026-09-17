b5ec966ccd84093670beaefdf0cf0bfefbb7ac31
Celso Junior <celsoestercio.jr@gmail.com>
2026-09-17 12:38:16 -0300

    First commit

---
+++ src/test/java/com/br/nutri/NutriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
